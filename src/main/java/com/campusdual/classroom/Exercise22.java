d2e52138a1deb5864cc43e96446b09576dd17f16
oscardasilvarodriguez <grelodj@hotmail.com>
2025-01-03 12:38:01 +0100

    Ejercicio java-22 resuelto

--- src/main/java/com/campusdual/classroom/Exercise22.java
+++ src/main/java/com/campusdual/classroom/Exercise22.java
@@ -7,18 +7,38 @@ import java.util.Set;
 public class Exercise22 {
 
     public static List<String> createArrayList() {
-
+        List<String> customList = new ArrayList<>();
+        customList.add("GKFFD");
+        customList.add("TNANA");
+        customList.add("MPMSL");
+        customList.add("PSWME");
+        customList.add("LZMLF");
+        customList.add("JYEBV");
+        customList.add("YELNT");
+        customList.add("JSNKR");
+        customList.add("JFESF");
+        customList.add("TMJLL");
+        return customList;
     }
 
     public static void printElementsAndIndex(List<String> customList) {
-
+        for (int i = 0; i < customList.size(); i++) {
+            System.out.println(i + " : " + customList.get(i));
+        }
     }
 
     public static boolean addElementToList(List<String> customList, String element) {
-
+        return customList.add(element);
     }
 
     public static void main(String[] args) {
+        List<String> arrayList = createArrayList();
+
+        boolean arrayListAdd = addElementToList(arrayList, "AAAAA\n");
+        System.out.println("Elemento añadido correctamente: " + (arrayListAdd ? "correcto" : "incorrecto"));
+
+        System.out.println("\nElementos en ArrayList: \n");
+        printElementsAndIndex(arrayList);
 
     }
 
